27ba914a73c45d25f0102854b0bd84664e272dd5
thuan-nv035 <54436704+thuan-nv035@users.noreply.github.com>
2022-05-18 13:29:11 +0700

    update

--- src/pages/Edit.js
+++ src/pages/Edit.js
@@ -1,21 +1,47 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { EditResponse } from "../services/EditResponse";
 
 const Edit = () => {
-    const [message, setMessage] = useState('')
-    const handleEdit = async () => {
-        
-    }
-    return (
-        <div>
-            <h1>Yêu cầu ngày chấm công</h1>
-            <input 
-            type = "text"
-            value = {message}
-            onChange = {(e) => setMessage(e.target.value)}
-            />
-            <button type = "button" onClick={() => handleEdit()}>submit</button>
+  const [message, setMessage] = useState("");
+  const navigate = useNavigate()
+  const handleEdit = async () => {
+    try {
+        await EditResponse.addRes(
+          JSON.stringify({
+            message: message,
+          })
+        ) 
+        alert("Gửi phẩn hồi thành công")
+          navigate("/user")
+      } catch (e) {
+        console.log(e);
+      }
+  };
+  return (
+    <div>
+      <div class="h-100 w-full flex items-center justify-center bg-teal-lightest font-sans">
+        <div class="bg-white rounded shadow p-6 m-4 w-full lg:w-3/4 lg:max-w-lg">
+          <div class="mb-4">
+            <h1 class="text-grey-darkest">Gửi phản hồi ngày công</h1>
+            <div class="flex mt-4">
+              <input
+                class="shadow appearance-none border rounded w-full py-2 px-3 mr-4 text-grey-darker"
+                placeholder=""
+                value={message}
+                onChange={(e) => setMessage(e.target.value)}
+              />
+              <button 
+              onClick={() => handleEdit()}
+              class="flex-no-shrink p-2 border-2 rounded text-teal border-teal hover:bg-teal">
+                Add
+              </button>
+            </div>
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default Edit;
--- src/pages/TakeBreak.js
+++ src/pages/TakeBreak.js
@@ -1,58 +1,72 @@
-import React, { useState, useEffect } from 'react';
-import {  useNavigate } from "react-router-dom";
-import { TakeBreakServices } from '../services/TakeBreakServices';
+import React, { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { TakeBreakServices } from "../services/TakeBreakServices";
 const TakeBreak = () => {
-    const [date, setDate] = useState('')
-    const [time, setTime] = useState('')
-    const [message, setMessage] = useState('')
-    const navigate = useNavigate()
+  const [date, setDate] = useState("");
+  const [time, setTime] = useState("");
+  const [message, setMessage] = useState("");
+  const navigate = useNavigate();
 
-    // useEffect(() => {
-    //     if (!localStorage.getItem("token")) {
-    //         alert("You must be logged in to access this page");
-    //         navigate("/");
-    //       }
-    // },[])
+  // useEffect(() => {
+  //     if (!localStorage.getItem("token")) {
+  //         alert("You must be logged in to access this page");
+  //         navigate("/");
+  //       }
+  // },[])
 
-    const handleSubmit = async () => {
-        console.log(date + time+ message);
-        try {
-            await TakeBreakServices.addBreak(
-              JSON.stringify({
-                date: date,
-                time: time,
-                message: message,
-              }),
-              JSON.parse(localStorage.getItem(""))
-            );
-          } catch (e) {
-            console.log(e);
-        }
+  const handleSubmit = async () => {
+    console.log(date + time + message);
+    try {
+      await TakeBreakServices.addBreak(
+        JSON.stringify({
+          date: date,
+          time: time,
+          message: message,
+        }),
+        JSON.parse(localStorage.getItem(""))
+      );
+    } catch (e) {
+      console.log(e);
     }
+  };
 
-    return (
-        <div>
-            <h1>Xin đi muộn</h1>
-            <div>
-                <input 
-                type="date" 
-                value = {date}
+  return (
+    <div>
+      <div class="h-100 w-full flex items-center justify-center bg-teal-lightest font-sans">
+        <div class="bg-white rounded shadow p-6 m-4 w-full lg:w-3/4 lg:max-w-lg">
+          <div class="mb-4">
+            <h1 class="text-grey-darkest text-center" >Xin đi muộn</h1>
+            <div class=" mt-4">
+              <input
+                type="date"
+                class="shadow my-2 appearance-none border rounded w-full py-2 px-3 mr-4 text-grey-darker"
+                value={date}
                 onChange={(e) => setDate(e.target.value)}
-                />
-                <input 
-                type = "time" 
-                value = {time}
-                onChange={(e) =>setTime(e.target.value)}
-                />
-                <input 
-                type = "text"
-                value = {message}
+              />
+              <input
+                type="time"
+                class="shadow my-2 appearance-none border rounded w-full py-2 px-3 mr-4 text-grey-darker"
+                value={time}
+                onChange={(e) => setTime(e.target.value)}
+              />
+              <input
+                type="text"
+                class="shadow my-2 appearance-none border rounded w-full py-2 px-3 mr-4 text-grey-darker"
+                placeholder="message"
+                value={message}
                 onChange={(e) => setMessage(e.target.value)}
-                />
-                <button type = "button" onClick={() => handleSubmit()}>submit</button>
+              />
+              <button 
+              onClick={() => handleSubmit()}
+              class="flex-no-shrink p-2 border-2 rounded text-teal border-teal  hover:bg-teal">
+                Submit
+              </button>
             </div>
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default TakeBreak;
